44642d623e749b2d2dea136a216c1340ae680792
phelixdusengimana <phelixdusengimana@gmail.com>
2022-03-20 15:08:26 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+       <h1>Let's <p>00393</p>seee.</h1>
+       <h1>This is cool, IMAO</h1>
+       <p>I want to buy MACBOOK pro.</p>
+    </div>
+  );
+}
+
+export default App;
